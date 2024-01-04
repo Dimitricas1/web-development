62d8a4d66e3944150a394508175666cc2d7db090
Dimitricas <dmitriymedvedok95@mail.ru>
2024-01-05 01:12:33 +0300

    Testing Tests working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,6 +27,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Tour of Heroes');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Toure of Heroes');
   });
 });
