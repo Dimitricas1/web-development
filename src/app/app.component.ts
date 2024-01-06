ddcfe65d39c7efd03fd9af42111f2fd63a0e95b0
Dimitricas <dmitriymedvedok95@mail.ru>
2024-01-07 02:21:36 +0300

    Добавлен CDK Scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoinComponent } from './coin/coin.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CoinComponent],
+  imports: [CommonModule, CoinComponent, ScrollingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers: [HttpClientModule]
+  providers: [HttpClientModule, ScrollingModule]
 })
 export class AppComponent {
   title = 'coinpaprika';
+  currencies = ['btc-bitcoin', 'eth-ethereum', 'usdt-tether', 'bnb-binance-coin', 'sol-solana', 'xrp-xrp'];
+  constructor(){}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 export const appConfig: ApplicationConfig = {
   providers: [importProvidersFrom([
-    HttpClient
+    HttpClient,
+    ScrollingModule
   ]),
   provideHttpClient()]
 };
