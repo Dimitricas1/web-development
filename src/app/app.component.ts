5b10872283634ab1ecdff15ee9a2b172efde678a
Dimitricas <dmitriymedvedok95@mail.ru>
2024-01-07 06:15:10 +0300

    Реализовано получение данных с сервера; в ESLint изменён перенос строки на CRLF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoinComponent } from './coin/coin.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatDividerModule } from '@angular/material/divider';
+import { GetcoinsService } from './services/getcoins.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CoinComponent, ScrollingModule, MatDividerModule],
+  imports: [CommonModule, CoinComponent, ScrollingModule, MatDividerModule, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [HttpClientModule, ScrollingModule]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'coinpaprika';
-  currencies = ['btc-bitcoin', 'eth-ethereum', 'usdt-tether', 'bnb-binance-coin', 'sol-solana', 'xrp-xrp'];
+  currencies: any;
   constructor(){}
+  coins = inject(GetcoinsService);
+
+  ngOnInit(): void {
+    this.getListCoins();
+  }
+
+  getListCoins(){
+    this.coins.getListCoins().subscribe(data => this.currencies = data);
+    console.log('error');
+  }
 }
--- src/app/services/getcoins.service.ts
+++ src/app/services/getcoins.service.ts
@@ -14,8 +14,13 @@ export class GetcoinsService {
   constructor() { }
   
   response: any;
+  statusCode: any;
 
   getCoinById(id: string) {
     return this.http.get(`${url}/coins/${id}`);
+  }
+
+  getListCoins(){
+    return this.http.get(`${url}/coins`);
   }
 }
